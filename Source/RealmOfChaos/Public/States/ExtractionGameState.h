be804753d9eb8f37acd60ace7c36b457ebbeed32
Connor <Kon_Priest@Hotmail.com>
2024-01-27 12:17:12 +0000

    Added States and created blueprints for gamemode scripts

---
+++ Source/RealmOfChaos/Public/States/ExtractionGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "ExtractionGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALMOFCHAOS_API AExtractionGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RealmOfChaos/Public/States/ExtractionPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ExtractionPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALMOFCHAOS_API AExtractionPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
