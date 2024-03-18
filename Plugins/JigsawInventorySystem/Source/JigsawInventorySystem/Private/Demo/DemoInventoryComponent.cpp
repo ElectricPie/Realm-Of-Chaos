5a5fe4e3b6b2ad49d7d1997a234ce56ed5090a3a
Connor Priest <Kon_Priest@Hotmail.com>
2024-03-18 12:41:19 +0000

    Move Jigsaw Inventory scripts to a Demo directory

--- Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Private/Demo/DemoInventoryComponent.cpp
+++ Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Private/Demo/DemoInventoryComponent.cpp
@@ -1,9 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "DemoInventoryComponent.h"
-
-#include "InventoryDemoItem.h"
+#include "Demo/DemoInventoryComponent.h"
+#include "Demo/InventoryDemoItem.h"
 
 
 // Sets default values for this component's properties
--- Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Private/Demo/InventoryDemoItem.cpp
+++ Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Private/Demo/InventoryDemoItem.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "InventoryDemoItem.h"
+#include "Demo/InventoryDemoItem.h"
 
 FInventoryDemoItem::FInventoryDemoItem()
 {
