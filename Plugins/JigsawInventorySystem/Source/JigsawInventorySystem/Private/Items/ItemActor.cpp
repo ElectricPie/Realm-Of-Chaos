b8054c14707f77f69a260d993a746e4fa9dba85b
Connor Priest <Kon_Priest@Hotmail.com>
2024-03-18 18:52:57 +0000

    Created Actor class for Items and Object class for storing Items

---
+++ Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Private/Items/ItemActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/ItemActor.h"
+
+// Sets default values
+AItemActor::AItemActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AItemActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+UItemObject* AItemActor::GetDefaultObject_Implementation()
+{
+	UE_LOG(LogTemp, Error, TEXT("%s has not implemented GetDefaultObject!"), *GetName());
+	return nullptr;
+}
+
+// Called every frame
+void AItemActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Plugins/JigsawInventorySystem/Source/JigsawInventorySystem/Public/Items/ItemActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemActor.generated.h"
+
+class UItemObject;
+
+UCLASS()
+class JIGSAWINVENTORYSYSTEM_API AItemActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	UItemObject* GetDefaultObject();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+	USceneComponent* Root;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(BlueprintReadOnly, Category="Item", meta=(AllowPrivateAccess="true"))
+	UItemObject* ItemObject;
+};
